52e22e4ec5bec86885bb5082abce5ddc3951e0a3
Jason Buckner <jason@jasonbuckner.com>
2020-07-15 10:50:14 -0700

    Export remaining classes

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export { LazyLoaderService } from './src/lazy-loader-service';
+export { LazyLoaderServiceInterface } from './src/lazy-loader-service-interface';
+export { BundleType } from './src/bundle-type';
